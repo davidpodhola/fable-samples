491d2c40638828f03b68b9ca089e6e47133dd783
David Podhola <david@podhola.net>
2016-02-23 20:07:20 +0100

    solution spread among different folders

---
+++ differentFolders/build/build.cmd
@@ -0,0 +1,9 @@
+call npm install
+
+set fable=../../../../others/Fabel
+
+..\..\tools\fnr\fnr.exe --cl --find "!!FABEL!!" --replace "%fable%" --dir "%cd%" --fileMask "fableconfig.json" --includeSubDirectories
+
+node %fable%/tools/fable2babel.js --projFile ../src/solution.fsx --outDir ../out
+
+
