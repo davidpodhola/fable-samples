585b401de64b4f85e1db2f9f9dc4a701ec104d4a
David Podhola <david@podhola.net>
2016-02-23 20:14:55 +0100

    path fix

--- differentFolders/build/build.cmd
+++ differentFolders/build/build.cmd
@@ -2,7 +2,7 @@ call npm install
 
 set fable=../../../../others/Fabel
 
-..\..\tools\fnr\fnr.exe --cl --find "!!FABEL!!" --replace "%fable%" --dir "%cd%" --fileMask "fableconfig.json" --includeSubDirectories
+..\..\tools\fnr\fnr.exe --cl --find "!!FABEL!!" --replace "%fable%" --dir "%cd%\.." --fileMask "fableconfig.json" --includeSubDirectories
 
 node %fable%/tools/fable2babel.js --projFile ../src/solution.fsx
 
