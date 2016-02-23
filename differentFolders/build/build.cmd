9697ded6e0b9e8b82f14eaffce32d8bb83dc911e
David Podhola <david@podhola.net>
2016-02-23 20:13:27 +0100

    removed the ignored --outDir switch from build.cmd

--- differentFolders/build/build.cmd
+++ differentFolders/build/build.cmd
@@ -4,6 +4,7 @@ set fable=../../../../others/Fabel
 
 ..\..\tools\fnr\fnr.exe --cl --find "!!FABEL!!" --replace "%fable%" --dir "%cd%" --fileMask "fableconfig.json" --includeSubDirectories
 
-node %fable%/tools/fable2babel.js --projFile ../src/solution.fsx --outDir ../out
+node %fable%/tools/fable2babel.js --projFile ../src/solution.fsx
+
 
 
